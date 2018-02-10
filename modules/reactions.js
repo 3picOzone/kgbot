413c0393360b0c771b6f0fd8ddcd272373704243
Benjamin-Weinberg <benweinberg01@gmail.com>
2018-02-10 13:21:21 -0600

    bug fix

--- modules/reactions.js
+++ modules/reactions.js
@@ -5,6 +5,7 @@ module.exports=
         if (!messageReaction.me) return;
         if (user.id == "393666734793424898") return;
         if (messageReaction.message.guild.roles.find(r => r.name.toLowerCase() == messageReaction.emoji.name.toLowerCase()) == undefined) return;
+        if (messageReaction.message.guild.members.find("id", user.id) == null) return;
         if (messageReaction.message.guild.members.find("id", user.id).roles.find(r => r.name.toLowerCase() == messageReaction.emoji.name.toLowerCase()))
         {
             messageReaction.message.guild.fetchMember(user)
