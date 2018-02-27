21ed0d74b5f608341df19110ed0fb2e5013e569c
3pic_Ozone <3pic.ozone@gmail.com>
2018-02-27 06:00:40 -0600

    update

---
+++ features/modPoke.js
@@ -0,0 +1,31 @@
+const settings = require('../settings.json');
+
+module.exports= 
+{
+    execute(oldMember, newMember)
+    {
+        console.log("modpoke running");
+        if (newMember.voiceChannel !== undefined && newMember.voiceChannel.name.toLowerCase().includes("applica")) 
+        {
+            if (  (newMember.voiceChannel !== undefined && (newMember.roles.exists("name", "Mod Leader") || 
+                                                            newMember.roles.exists("name", "Moderator") || 
+                                                            newMember.roles.exists("name", "Officer") || 
+                                                            newMember.roles.exists("name", "Clan Leader") || 
+                                                            newMember.roles.exists("name", "Technician") 
+                                                            )
+                    )
+            ) return; // DONT LOG MODS JOINING THE ROOMS
+
+            var currentGuild = undefined; 
+            var currentMember = undefined;
+            if (newMember.voiceChannel !== undefined)
+            {
+                currentMember = newMember;
+                currentGuild = newMember.voiceChannel.guild;
+            }
+
+            currentGuild.channels.find("name", "application-notifications").send(currentGuild.roles.find("name", "Moderator") + " A user has joined the application channel: " + currentMember);
+        }
+        else return;
+    },   
+};      
