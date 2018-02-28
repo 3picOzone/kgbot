81f0cbd7824360d860818f7154d69f38922993c6
Benjamin-Weinberg <benweinberg01@gmail.com>
2018-02-28 16:25:39 -0600

    fix

--- commands/Core/help.js
+++ commands/Core/help.js
@@ -10,7 +10,7 @@ module.exports = {
 		message.reply("Command currently disabled, but will be back soon!");
 		// const { commands } = message.client;
 		// const data = [];
-
+		//
 		// if (!args.length) {
 		// 	data.push('**Here\'s a list of all my commands:**');
 		// 	data.push(commands.map(command => command.name).join(', '));
