26fccd8d7502301cc0a998e5dc7fa4ae15290af5
Benjamin-Weinberg <benweinberg01@gmail.com>
2018-03-03 23:35:58 -0600

    test connections

--- commands/ted/ted.js
+++ commands/ted/ted.js
@@ -8,25 +8,18 @@ module.exports = {
     cooldown: 0,                            // Optional Cooldown Between Uses
     args: true,                            // true/false are there any args for this command?
     guildOnly: true,                       // true/false should it only be used in guild channels and not in PM's
-    execute(message, args) {        		// Function Goes Here
+    execute(message, args, client, connection) {        		// Function Goes Here
         if (message.mentions.members.first() == undefined) return message.reply("You must mention at least one member!");
         
-        var mysql = require('mysql');
-        var con = mysql.createConnection({
-            host: settings.dbHost,
-            user: settings.dbUsername,
-            password: settings.dbPassword,
-            database: settings.dbDatabase
-        });
 
-        con.connect(function(err) {
+        connection.connect(function(err) {
             if (err) 
             {
                 console.log(err.stack);
                 return message.channel.send("There was a Database Error when doing this command, please contact a Tech!");
             }
             sql = "SELECT * FROM teds WHERE userid = "+message.mentions.members.first().id;
-            con.query(sql, function (err, result) {
+            connection.query(sql, function (err, result) {
                 if (err)
                 {
                     console.log(err.stack);
@@ -42,7 +35,7 @@ module.exports = {
                     {
                         if(args[1] == undefined) return message.reply("This user does not have a TED link set yet, please set one!");
                         sql = "INSERT INTO teds (userid, username, tedlink) VALUES ('" + message.mentions.members.first().id + "', '" + message.mentions.members.first().user.username + "#" + message.mentions.members.first().user.discriminator + "', '" + args[1] +"');";
-                        con.query(sql, function (err, result) {
+                        connection.query(sql, function (err, result) {
                             if (err)
                             {
                                 console.log(err.stack);
@@ -62,7 +55,7 @@ module.exports = {
                         else
                         {
                             sql = "UPDATE teds SET tedlink = '" + args[1] + "'WHERE userid = '" + message.mentions.members.first().id + "';";
-                            con.query(sql, function (err, result) {
+                            connection.query(sql, function (err, result) {
                                 if (err)
                                 {
                                     console.log(err.stack);
@@ -78,8 +71,7 @@ module.exports = {
                     }
                 }
             });
-        });
-        con.end();   // remove if crashes do not get fixed  
+        });  
 	},
 };
 
