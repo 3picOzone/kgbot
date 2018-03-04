26b14bd29944c1dffb8dba012e58ef9dc19543fd
3pic_Ozone <3pic.ozone@gmail.com>
2018-03-03 23:40:52 -0600

    update

--- commands/ted/ted.js
+++ commands/ted/ted.js
@@ -10,68 +10,59 @@ module.exports = {
     guildOnly: true,                       // true/false should it only be used in guild channels and not in PM's
     execute(message, args, client, connection) {        		// Function Goes Here
         if (message.mentions.members.first() == undefined) return message.reply("You must mention at least one member!");
-        
-
-        connection.query(function(err) {
-            if (err) 
+        sql = "SELECT * FROM teds WHERE userid = "+message.mentions.members.first().id;
+        connection.query(sql, function (err, result) {
+            if (err)
             {
                 console.log(err.stack);
                 return message.channel.send("There was a Database Error when doing this command, please contact a Tech!");
             }
-            sql = "SELECT * FROM teds WHERE userid = "+message.mentions.members.first().id;
-            connection.query(sql, function (err, result) {
-                if (err)
+            if (result[0] == undefined)         // user doesnt have a ted currently
+            {
+                if (!(message.member.roles.exists("name", "Technician") ||  message.member.roles.exists("name", "Mod Leader")  ||  message.member.roles.exists("name", "Moderator")  ||  message.member.roles.exists("name", "Officer") ||  message.member.roles.exists("name", "Clan Leader")) )
                 {
-                    console.log(err.stack);
-                    return message.channel.send("There was a Database Error when doing this command, please contact a Tech!");
+                    return message.reply("This user does not have a TED link set yet, please contact a moderator to fix this!")
                 }
-                if (result[0] == undefined)         // user doesnt have a ted currently
+                else
+                {
+                    if(args[1] == undefined) return message.reply("This user does not have a TED link set yet, please set one!");
+                    sql = "INSERT INTO teds (userid, username, tedlink) VALUES ('" + message.mentions.members.first().id + "', '" + message.mentions.members.first().user.username + "#" + message.mentions.members.first().user.discriminator + "', '" + args[1] +"');";
+                    connection.query(sql, function (err, result) {
+                        if (err)
+                        {
+                            console.log(err.stack);
+                            return message.channel.send("There was a Database Error when doing this command, please contact a Tech!");
+                        }
+                        message.channel.send("TED created for " + message.mentions.members.first().user.username);
+                    }); 
+                }
+            }
+            else{                               // has a ted already, trying to update link?
+                if(args[1] != undefined)
                 {
                     if (!(message.member.roles.exists("name", "Technician") ||  message.member.roles.exists("name", "Mod Leader")  ||  message.member.roles.exists("name", "Moderator")  ||  message.member.roles.exists("name", "Officer") ||  message.member.roles.exists("name", "Clan Leader")) )
                     {
-                        return message.reply("This user does not have a TED link set yet, please contact a moderator to fix this!")
+                        return message.reply("Only Moderators can update TED links, please contact someone Moderator or above to fix this")
                     }
                     else
                     {
-                        if(args[1] == undefined) return message.reply("This user does not have a TED link set yet, please set one!");
-                        sql = "INSERT INTO teds (userid, username, tedlink) VALUES ('" + message.mentions.members.first().id + "', '" + message.mentions.members.first().user.username + "#" + message.mentions.members.first().user.discriminator + "', '" + args[1] +"');";
+                        sql = "UPDATE teds SET tedlink = '" + args[1] + "'WHERE userid = '" + message.mentions.members.first().id + "';";
                         connection.query(sql, function (err, result) {
                             if (err)
                             {
                                 console.log(err.stack);
                                 return message.channel.send("There was a Database Error when doing this command, please contact a Tech!");
                             }
-                            message.channel.send("TED created for " + message.mentions.members.first().user.username);
+                            message.channel.send("TED link updated for " + message.mentions.members.first().user.username);
                         }); 
                     }
                 }
-                else{                               // has a ted already, trying to update link?
-                    if(args[1] != undefined)
-                    {
-                        if (!(message.member.roles.exists("name", "Technician") ||  message.member.roles.exists("name", "Mod Leader")  ||  message.member.roles.exists("name", "Moderator")  ||  message.member.roles.exists("name", "Officer") ||  message.member.roles.exists("name", "Clan Leader")) )
-                        {
-                            return message.reply("Only Moderators can update TED links, please contact someone Moderator or above to fix this")
-                        }
-                        else
-                        {
-                            sql = "UPDATE teds SET tedlink = '" + args[1] + "'WHERE userid = '" + message.mentions.members.first().id + "';";
-                            connection.query(sql, function (err, result) {
-                                if (err)
-                                {
-                                    console.log(err.stack);
-                                    return message.channel.send("There was a Database Error when doing this command, please contact a Tech!");
-                                }
-                                message.channel.send("TED link updated for " + message.mentions.members.first().user.username);
-                            }); 
-                        }
-                    }
-                    else
-                    {
-                        message.channel.send("TED Link for " + result[0].username + ": " + result[0].tedlink);
-                    }
+                else
+                {
+                    message.channel.send("TED Link for " + result[0].username + ": " + result[0].tedlink);
                 }
-            });
-        });  
+            }
+        });
 	},
 };
 
