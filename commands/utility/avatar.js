ade05e422cd982fcd121d6485d8ad2f66833780d
3pic_Ozone <3pic.ozone@gmail.com>
2018-03-07 21:47:12 -0600

    rewrite and update

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -1,13 +1,13 @@
 const settings = require('../../settings.json');   										// THIS FILE NEEDS TO BE IN THE COMMANDS FOLDER!!!!!!!
 
 module.exports = {
-	name: 'avatar',                  	 											// Command name (same as the file.js name)
-	description: 'Get the avatar URL of the tagged user(s), or your own avatar', 							// info that gets pulled by the help command for a description
-	aliases: ['icon','pfp'],  																// Optional saiases for the command
+	name: 'avatar',                  	 												// Command name (same as the file.js name)
+	description: 'Get the avatar URL of the tagged user(s), or your own avatar',		// info that gets pulled by the help command for a description
+	aliases: ['icon','pfp'],  															// Optional saiases for the command
 	usage: '[@user]',                													// For help command or if command was sent wrong
 	requiredRoles: [''],																// an array of role names that are required to run the command or (false || ['']) to disable
 	cooldown: 0,                            											// Optional Cooldown Between Uses (defaults to 3 seconds if none set)
-    args: true,                            												// true/false are there any args for this command?
+    args: false,                           												// true/false are there any args for this command?
 	guildOnly: false,                       											// true/false should it only be used in guild channels and not in PM's
 	ownerOnly: false,																	// should this command be only used by the bot owner (3pic_Ozone)
 	hidden: false,                                                                      // should this command be hidden from the help menu
