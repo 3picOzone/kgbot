9039df21155be830a6b0de08f5cf562233bef87b
Benjamin-Weinberg <benweinberg01@gmail.com>
2018-03-10 14:25:48 -0600

    update ad add features

---
+++ features/streamers.js
@@ -0,0 +1,16 @@
+module.exports= 
+{
+    async execute(oldMember,newMember)
+    {
+        if (!(newMember.guild && newMember.voiceChannel && newMember.user.presence.game) && newMember.roles.exists("name", "NOW LIVE!")) return await newMember.removeRole(newMember.guild.roles.find("name", "NOW LIVE!"));
+        if (!(newMember.guild && newMember.voiceChannel && newMember.user.presence.game)) return;
+        if (newMember.roles.exists("name", "KG Streamer") && newMember.user.presence.game.streaming && (newMember.voiceChannel.guild.name == "KG Test Server"))
+        {
+            if (!newMember.roles.exists("name", "NOW LIVE!")) return await newMember.addRole(newMember.guild.roles.find("name", "NOW LIVE!"));
+        }
+        else if (newMember.roles.exists("name", "NOW LIVE!"))
+        {
+            return await newMember.removeRole(newMember.guild.roles.find("name", "NOW LIVE!"));
+        }
+    },   
+};      
