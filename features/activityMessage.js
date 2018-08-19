f0bcd9a7b608be139514b09a6e6946a938941461
Benjamin-Weinberg <benweinberg01@gmail.com>
2018-08-19 00:21:40 -0500

    attempt

--- features/activityMessage.js
+++ features/activityMessage.js
@@ -15,45 +15,36 @@ module.exports=
 
         activities.execute(message.member, message.channel, connection, "message");
 
-        // sql = "INSERT INTO events (Type, userid, channelid, parentid, parentname) VALUES ('message', '" + message.author.id + "', '" + message.channel.id + "', '" + parentChannel.id + "' , '" + parentChannel.name.replace(/\W/g, '') + "');";
-        // connection.query(sql, function (err, results) {
+        sql = "SELECT * FROM activity WHERE sectionID = "+parentChannel.id;
+        // connection.query(sql, function (err, result) {
         //     if (err)
         //     {
         //         console.log(err.stack);
-        //         return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a message to events table");
+        //         return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to update activities!");
         //     }
-        // }); 
-
-        sql = "SELECT * FROM activity WHERE sectionID = "+parentChannel.id;
-        connection.query(sql, function (err, result) {
-            if (err)
-            {
-                console.log(err.stack);
-                return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to update activities!");
-            }
-            if (result[0] == undefined)         // no activity yet
-            {
-                sql = "INSERT INTO activity (sectionid, sectionName, activityVoice, activityMessage, totalActivityVoice, totalActivityMessage) VALUES ('" + parentChannel.id + "', '" + parentChannel.name.replace(/\W/g, '') + "', '1','0','1','0');";
-                connection.query(sql, function (err, results) {
-                    if (err)
-                    {
-                        console.log(err.stack);
-                        return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a section to activities");
-                    }
-                }); 
-            }
-            else{                               // has a activity already,update it
-                newActivity = parseInt(result[0].activityMessage, 10) + 1;
-                newActivityTotal = parseInt(result[0].totalActivityMessage, 10) + 1;
-                sql = "UPDATE activity SET activityMessage = '" + newActivity + "', totalActivityMessage = '" + newActivityTotal + "' WHERE sectionid = '" + parentChannel.id + "';";
-                connection.query(sql, function (err, results) {
-                    if (err)
-                    {
-                        console.log(err.stack);
-                        return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when trying to update section activities!");
-                    }
-                }); 
-            }
-        });
+        //     if (result[0] == undefined)         // no activity yet
+        //     {
+        //         sql = "INSERT INTO activity (sectionid, sectionName, activityVoice, activityMessage, totalActivityVoice, totalActivityMessage) VALUES ('" + parentChannel.id + "', '" + parentChannel.name.replace(/\W/g, '') + "', '1','0','1','0');";
+        //         connection.query(sql, function (err, results) {
+        //             if (err)
+        //             {
+        //                 console.log(err.stack);
+        //                 return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a section to activities");
+        //             }
+        //         }); 
+        //     }
+        //     else{                               // has a activity already,update it
+        //         newActivity = parseInt(result[0].activityMessage, 10) + 1;
+        //         newActivityTotal = parseInt(result[0].totalActivityMessage, 10) + 1;
+        //         sql = "UPDATE activity SET activityMessage = '" + newActivity + "', totalActivityMessage = '" + newActivityTotal + "' WHERE sectionid = '" + parentChannel.id + "';";
+        //         connection.query(sql, function (err, results) {
+        //             if (err)
+        //             {
+        //                 console.log(err.stack);
+        //                 return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when trying to update section activities!");
+        //             }
+        //         }); 
+        //     }
+        // });
     },   
 };
