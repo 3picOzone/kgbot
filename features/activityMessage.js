8cb4f1a67424b8e5396f11417d043f1e9b567b37
3pic_Ozone <3pic.ozone@gmail.com>
2018-08-18 23:25:34 -0500

    make universal function

--- features/activityMessage.js
+++ features/activityMessage.js
@@ -1,4 +1,5 @@
 const settings = require('../settings.json');
+const activities = require('./activities.js');
 
 module.exports= 
 {
@@ -8,17 +9,20 @@ module.exports=
         if (message.channel.type != 'text') return;
         if (message.channel.parent == undefined) return;
         if (message.guild.name != 'Konvict Gaming') return;
+        if (!message.member) return;
         
         const parentChannel = message.channel.parent;
 
-        sql = "INSERT INTO events (Type, userid, channelid, parentid, parentname) VALUES ('message', '" + message.author.id + "', '" + message.channel.id + "', '" + parentChannel.id + "' , '" + parentChannel.name.replace(/\W/g, '') + "');";
-        connection.query(sql, function (err, results) {
-            if (err)
-            {
-                console.log(err.stack);
-                return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a message to events table");
-            }
-        }); 
+        activities.execute(message.member, message.channel, connection, "message");
+
+        // sql = "INSERT INTO events (Type, userid, channelid, parentid, parentname) VALUES ('message', '" + message.author.id + "', '" + message.channel.id + "', '" + parentChannel.id + "' , '" + parentChannel.name.replace(/\W/g, '') + "');";
+        // connection.query(sql, function (err, results) {
+        //     if (err)
+        //     {
+        //         console.log(err.stack);
+        //         return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a message to events table");
+        //     }
+        // }); 
 
         sql = "SELECT * FROM activity WHERE sectionID = "+parentChannel.id;
         connection.query(sql, function (err, result) {
