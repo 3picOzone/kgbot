ea08c5bcf8da9a28be9703f4fda3af52b6036105
Benjamin-Weinberg <benweinberg01@gmail.com>
2018-08-19 02:32:20 -0500

    fix

--- commands/core/activity.js
+++ commands/core/activity.js
@@ -13,7 +13,7 @@ module.exports = {
 	ownerOnly: false,																	// should this command be only used by the bot owner (3pic_Ozone)
 	hidden: true,                                                                       // should this command be hidden from the help menu
 	disabled: false,																	// should this command be available to be used
-	async execute(message, args, connection)         									// Function Goes Here
+    execute(message, args, connection)         									// Function Goes Here
 	{  
         var sql; 
         var numParents;
@@ -86,7 +86,7 @@ module.exports = {
                                     console.log(err.stack);
                                     return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to get events from events table");
                                 }
-                                await embed.addField("__" + message.guild.channels.get(currentid).name.replace(/\W/g, '') + ":__", result.length);
+                                embed.addField("__" + message.guild.channels.get(currentid).name.replace(/\W/g, '') + ":__", result.length);
                             }); 
                             k++;
                         }
