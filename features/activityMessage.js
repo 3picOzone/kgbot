5152ea32cefad681146ffd93e0530d809c8a2683
3pic_Ozone <3pic.ozone@gmail.com>
2018-08-29 17:37:48 -0500

    fix bug

--- features/activityMessage.js
+++ features/activityMessage.js
@@ -9,42 +9,12 @@ module.exports=
         if (message.channel.type != 'text') return;
         if (message.channel.parent == undefined) return;
         if (message.guild.name != 'Konvict Gaming') return;
+        if (message.guild.id != '390650534551617546') return;
         if (!message.member) return;
         
         const parentChannel = message.channel.parent;
 
         activities.execute(message.member, message.channel, connection, "message");
 
-        sql = "SELECT * FROM activity WHERE sectionID = "+parentChannel.id;
-        // connection.query(sql, function (err, result) {
-        //     if (err)
-        //     {
-        //         console.log(err.stack);
-        //         return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to update activities!");
-        //     }
-        //     if (result[0] == undefined)         // no activity yet
-        //     {
-        //         sql = "INSERT INTO activity (sectionid, sectionName, activityVoice, activityMessage, totalActivityVoice, totalActivityMessage) VALUES ('" + parentChannel.id + "', '" + parentChannel.name.replace(/\W/g, '') + "', '1','0','1','0');";
-        //         connection.query(sql, function (err, results) {
-        //             if (err)
-        //             {
-        //                 console.log(err.stack);
-        //                 return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a section to activities");
-        //             }
-        //         }); 
-        //     }
-        //     else{                               // has a activity already,update it
-        //         newActivity = parseInt(result[0].activityMessage, 10) + 1;
-        //         newActivityTotal = parseInt(result[0].totalActivityMessage, 10) + 1;
-        //         sql = "UPDATE activity SET activityMessage = '" + newActivity + "', totalActivityMessage = '" + newActivityTotal + "' WHERE sectionid = '" + parentChannel.id + "';";
-        //         connection.query(sql, function (err, results) {
-        //             if (err)
-        //             {
-        //                 console.log(err.stack);
-        //                 return message.guild.channels.find('name', 'tech-talk').send("There was a Database Error when trying to update section activities!");
-        //             }
-        //         }); 
-        //     }
-        // });
     },   
 };
--- features/activityVoice.js
+++ features/activityVoice.js
@@ -8,47 +8,12 @@ module.exports=
         if (newMember.voiceChannel == undefined) return;
         if (oldMember.voiceChannel == newMember.voiceChannel) return;
         if(newMember.voiceChannel.parent == undefined) return;
+        if (newMember.guild.id != '390650534551617546') return;
         if (newMember.voiceChannel !== undefined && newMember.guild.name == 'Konvict Gaming')      //currntly only for KG
         {
             const parentChannel = newMember.voiceChannel.parent;
 
             activities.execute(newMember, newMember.voiceChannel, connection, "voice");
-
-            // sql = "SELECT * FROM activity WHERE sectionID = "+parentChannel.id;
-            // connection.query(sql, function (err, result) {
-            //     if (err)
-            //     {
-            //         console.log(err.stack);
-            //         return newMember.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to update activities!");
-            //     }
-            //     if (result[0] == undefined)         // no activity yet
-            //     {
-            //         sql = "INSERT INTO activity (sectionid, sectionName, activityVoice, activityMessage, totalActivityVoice, totalActivityMessage) VALUES ('" + parentChannel.id + "', '" + parentChannel.name.replace(/\W/g, '') + "', '1','0','1','0');";
-            //         connection.query(sql, function (err, results) {
-            //             if (err)
-            //             {
-            //                 console.log(err.stack);
-            //                 return newMember.guild.channels.find('name', 'tech-talk').send("There was a Database Error when attempting to add a section to activities");
-            //             }
-            //         }); 
-            //     }
-            //     else{                               // has a activity already,update it
-            //         newActivity = parseInt(result[0].activityVoice, 10) + 1;
-            //         newActivityTotal = parseInt(result[0].totalActivityVoice, 10) + 1;
-            //         sql = "UPDATE activity SET activityVoice = '" + newActivity + "', totalActivityVoice = '" + newActivityTotal + "' WHERE sectionid = '" + parentChannel.id + "';";
-
-            //         // "UPDATE activity SET activityVoice = '" + newActivity + ", totalActivityVoice = '" + newActivityTotal + "' WHERE sectionid = '" + parentChannel.id + "';"
-
-
-            //         connection.query(sql, function (err, results) {
-            //             if (err)
-            //             {
-            //                 console.log(err.stack);
-            //                 return newMember.guild.channels.find('name', 'tech-talk').send("There was a Database Error when trying to update section activities!");
-            //             }
-            //         }); 
-            //     }
-            // });
         }
     },   
 };
